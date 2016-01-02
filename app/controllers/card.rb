503f481d6fce101e8272457e74765905bfe824d2
luisplaz <luisplaz@hotmail.com>
2016-01-02 15:59:01 -0500

    Add changes to controllers

--- app/controllers/card.rb
+++ app/controllers/card.rb
@@ -1,2 +1,5 @@
-
+post '/cards/play/:id' do
+	binding.pry
+	erb :'cards/show'
+end
 
--- app/controllers/deck.rb
+++ app/controllers/deck.rb
@@ -8,3 +8,13 @@ get '/decks/:id' do
 	@all_deck_cards = @deck.cards
 	erb :'cards/show'
 end
+
+post '/decks/guess/:id' do
+	@card= Card.find_by(id: params[:id])
+	@user_answer = params[:flashcard][:answer]
+	@guess = Guess.create(number_plays: +1,card_id: @card.id)
+	binding.pry
+	redirect "/"
+
+	
+end
