6a8fde72369fed3d848aa8209385983220c0fe5d
Sasha Tlr <sashatailor@gmail.com>
2016-01-03 23:15:36 -0500

    Add end of round stats

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,6 +1,8 @@
 class Card < ActiveRecord::Base
   belongs_to :deck
   has_many :guesses
+  has_many :rounds, through: :guesses
+  has_many :users, through: :guesses
 
   def check_card_against_guess?(guess_string)
     self.answer == guess_string
--- db/migrate/20160102112648_create_guesses.rb
+++ db/migrate/20160102112648_create_guesses.rb
@@ -1,7 +1,7 @@
 class CreateGuesses < ActiveRecord::Migration
 	def change
 		create_table :guesses do |t|
-			t.integer :number_plays, default: 0
+			t.integer :number_plays, default: 1
 			t.integer :round_id, index: true
 			t.integer :card_id, index: true
 			t.boolean :correct_guess, null: false, default: false
